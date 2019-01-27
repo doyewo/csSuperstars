fa4734afba81fffc2323dd6632327de1f518081f
christopherjose <cjose@luc.edu>
2019-01-26 18:44:54 -0600

    updated TweetParser class from Ramsha

--- src/main/TweetParser.java
+++ src/main/TweetParser.java
@@ -1,30 +1,34 @@
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.lang.String;
 
 public class TweetParser {
 
     String urlRegex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
     Pattern pattern = Pattern.compile(urlRegex);
-    
+
     String mentionRegex = "/^(?!.*\\bRT\\b)(?:.+\\s)?@\\w+/i";
     Pattern pattern1  = Pattern.compile(mentionRegex);
 
     String hashtagRegex = "\\B(\\#[a-zA-Z]+\\b)(?!;)";
     Pattern pattern2 = Pattern.compile((hashtagRegex));
 
+    Matcher matcher = pattern.matcher("@Sarah loves cars.");
+    Matcher matcher1 = pattern1.matcher("#Cars are awesome");
+    Matcher matcher2 = pattern2.matcher("Visit https://www.cars.com for more cars") ;
+
     private String tweet;
 
     public TweetParser(String tweet) {
-        String input = "@Sarah love cars." ;
-        String regex = "/^(?!.*\\bRT\\b)(?:.+\\s)?@\\w+/i";
-        Matcher matchMention = Pattern.compile(regex).matchMention(input);
-        if (matcher.find()) {
-            System.out.println(matcher.group(0));
+        if (mentionRegex.contains("@Sarah")) {
+            System.out.println(pattern);
+        }  else if(hashtagRegex.contains("#Cars")) {
+            System.out.println(pattern1);
+        } else if(urlRegex.contains("http")) {
+            System.out.println(pattern2);
         }
-    }
-}
-
